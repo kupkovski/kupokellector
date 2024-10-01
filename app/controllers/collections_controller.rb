24b7564966791d1b0c2fac3d779599f4caff9121
André Kupkovski <kupkovski@gmail.com>
2024-10-01 20:20:10 -0300

    starting collection controller/views

--- app/controllers/collections_controller.rb
+++ app/controllers/collections_controller.rb
@@ -1,3 +1,9 @@
 class CollectionsController < ApplicationController
+  def index
+    @collections = Collection.order(released_at: :desc).all
+  end
 
+  def show
+    @collection = Collection.find(params[:id])
+  end
 end
