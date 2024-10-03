e5dbda46b94c3f66621855b17651f5452d79d872
André Kupkovski <kupkovski@gmail.com>
2024-10-03 14:36:12 -0300

    first implementation of collect and discard with turbo

--- app/controllers/cards_controller.rb
+++ app/controllers/cards_controller.rb
@@ -1,4 +1,34 @@
 class CardsController < ApplicationController
+  include ActionView::RecordIdentifier
+
+  before_action :set_card
+
   def collect
+
+    current_user.cards << @card
+
+    render turbo_stream: turbo_stream.replace(
+      dom_id(@card),
+      partial: "collections/card",
+      locals: { card: @card }
+    )
+  end
+
+  def discard
+    if current_user.cards.find(@card.id)
+      current_user.cards.destroy(@card)
+    end
+
+    render turbo_stream: turbo_stream.replace(
+      dom_id(@card),
+      partial: "collections/card",
+      locals: { card: @card }
+    )
+  end
+
+  private
+
+  def set_card
+    @card = Card.find(params[:id])
   end
 end
